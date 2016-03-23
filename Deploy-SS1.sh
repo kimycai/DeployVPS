4c16ec5ef54ff63d04e8c8e9e1b9e7f439c353a1
kimycai <kimycai@qq.com>
2016-03-23 20:12:41 +0800

    Update Deploy-SS1.sh

--- Deploy-SS1.sh
+++ Deploy-SS1.sh
@@ -7,7 +7,10 @@ apt-get install python-pip
 pip install shadowsocks
 
 IP=$(curl -s -4 icanhazip.com)
-    
+    if [[ "$IP" = "" ]]; then
+        IP=$(curl -s -4 ipinfo.io/ip)
+    fi
+    echo -e "Your main public IP is\t\033[32m$IP\033[0m"
 
 cat >> /etc/shadowsocks.json <<-EOF
 {
