ea27d302b52b3dfa368e37d4a00790bc883b9ba0
kimycai <kimycai@qq.com>
2016-03-25 14:44:54 +0800

    Update DPVultrSS.sh

--- DPVultrSS.sh
+++ DPVultrSS.sh
@@ -56,3 +56,9 @@ net.ipv4.tcp_congestion_control = hybla
 EOF
 
 sysctl -p
+
+echo -e "Your main public IP is\t\033[32m$IP\033[0m"
+echo -e "server_port:\t\033[32m443\033[0m"
+echo -e "local_port:\t\033[32m1080\033[0m"
+echo -e "password:\t\033[32mCym9631514404\033[0m"
+echo -e "method:\t\033[32mrc4-md5\033[0m"
