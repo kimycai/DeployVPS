20f180f52b60d8eb7afe75e4d3262242828564cf
kimycai <kimycai@qq.com>
2016-03-28 20:57:16 +0800

    for Ubuntu 14 32bit

---
+++ OpenVPNAS.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install iptables
+wget http://swupdate.openvpn.org/as/openvpn-as-2.0.25-Ubuntu14.i386.deb
+sudo dpkg -i openvpn-as-2.0.25-Ubuntu14.i386.deb
+sudo passwd Cym9631514404
