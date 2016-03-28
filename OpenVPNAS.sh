84918adfc85c64f0ba8a5cb47996df4b50b422ad
kimycai <kimycai@qq.com>
2016-03-28 22:50:22 +0800

    Update OpenVPNAS.sh

--- OpenVPNAS.sh
+++ OpenVPNAS.sh
@@ -1,4 +1,5 @@
 
-wget http://swupdate.openvpn.org/as/openvpn-as-2.0.25-Ubuntu14.i386.deb
-sudo dpkg -i openvpn-as-2.0.25-Ubuntu14.i386.deb
+wget http://swupdate.openvpn.org/as/openvpn-as-2.0.25-Ubuntu12.i386.deb
+sudo dpkg -i openvpn-as-2.0.25-Ubuntu12.i386.deb
 sudo passwd openvpn
+
