006cf2b6ddd14587b71cc770cb9534559e4f9d49
kimycai <kimycai@qq.com>
2016-03-29 00:20:11 +0800

    Update CentOpenVPNAS.sh

--- CentOpenVPNAS.sh
+++ CentOpenVPNAS.sh
@@ -3,6 +3,4 @@ curl -O http://swupdate.openvpn.org/as/openvpn-as-2.0.25-CentOS6.i386.rpm
 
 sudo rpm -i openvpn-as-2.0.25-CentOS6.i386.rpm
 
-sudo passwd 9631514404
-
 /usr/local/openvpn_as/bin/ovpn-init
