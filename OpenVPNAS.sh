9b76be951366a9c6e8200b1bf29934497ccf5b09
kimycai <kimycai@qq.com>
2016-03-28 22:44:45 +0800

    Update OpenVPNAS.sh

--- OpenVPNAS.sh
+++ OpenVPNAS.sh
@@ -1,6 +1,4 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get -y install iptables
+
 wget http://swupdate.openvpn.org/as/openvpn-as-2.0.25-Ubuntu14.i386.deb
 sudo dpkg -i openvpn-as-2.0.25-Ubuntu14.i386.deb
 sudo passwd openvpn
