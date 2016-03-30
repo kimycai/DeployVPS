e807f2baf3f6da8ad4ae76ba0e0916a87bd3ceda
kimycai <kimycai@qq.com>
2016-03-30 18:58:26 +0800

    Update DO-UBT-VPN.sh

--- DO-UBT-VPN.sh
+++ DO-UBT-VPN.sh
@@ -2,6 +2,10 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+#初始化配置
+echo "choose (1) to bind to all interfaces"
+echo "/usr/local/openvpn_as/bin/ovpn-init"
+
 #获取服务包
 wget --no-check-certificate http://swupdate.openvpn.org/as/openvpn-as-2.0.25-Ubuntu14.i386.deb
 
@@ -11,7 +15,6 @@ dpkg -i openvpn-as-2.0.25-Ubuntu14.i386.deb
 #设置登录密码
 passwd openvpn
 
-#初始化配置
-echo "input "DELETE" and enter"
-echo "choose (1) to bind to all interfaces"
-#/usr/local/openvpn_as/bin/ovpn-init
+
+
+
